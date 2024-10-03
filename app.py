7c5192ab0987890a0bd2b6cf9984053e763189c0
amryyahya <amryyahya07@gmail.com>
2024-10-03 02:25:19 +0000

    expenses delete route

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from database import users
 from default_categories import DEFAULT_CATEGORIES
+import uuid
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'your-secret-key'
@@ -17,12 +18,10 @@ def register():
     password = data.get('password')
     if not username or not password:
         return jsonify({"msg": "Username and password are required"}), 400
-
     for user in allUsers:
         print(user["username"])
         if user["username"] == username:
             return jsonify({"msg": "username has already exist"}), 400
-    
     user = {
         "username":username,
         "password":password,
@@ -37,14 +36,11 @@ def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-
     if not username or not password:
         return jsonify({"msg": "Username and password are required"}), 400
-
     user = users.find_one({"username":username})
     if not user or not password == password:
         return jsonify({"msg": "Invalid username or password"}), 401
-
     # Create a new access token
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token), 200
@@ -54,24 +50,26 @@ def login():
 @jwt_required()
 def protected():
     current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+    return jsonify(logged_in_as=current_user), 2
 
+################ EXPENSE ROUTE ################################################################
 @app.route('/expenses', methods=['GET'])
-def get_example():
-    return jsonify({
-        'message': 'This is a GET request!',
-        'parameters': request.args
-    })
+@jwt_required()
+def getAllExpenses():
+    current_user = get_jwt_identity()
+    user = users.find_one({"username":current_user})
+    return jsonify({"expenses": user.get("expenses")}),200
 
 @app.route('/expenses', methods=['POST'])
 @jwt_required()
-def updateExpense():
+def addExpense():
     current_user = get_jwt_identity()
     data = request.get_json()
     amount = data.get('amount')
     category = data.get('category')
     description = data.get('description')
     newExpense = {
+        "_id": str(uuid.uuid4()),  
         "amount": amount,
         "category": category,
         "description": description,
@@ -81,6 +79,20 @@ def updateExpense():
         { "username": current_user },  
         { "$push": { "expenses": newExpense } }
     )
+    return jsonify(status="expense record added"), 200
+
+@app.route('/expenses', methods=['DELETE'])
+@jwt_required()
+def deleteExpense():
+    current_user = get_jwt_identity()
+    data = request.get_json()
+    expenseId = data.get('_id')
+    print(expenseId)
+    users.update_one(
+        { "username": current_user},
+        { "$pull": { "expenses": { "_id": expenseId}}}
+    )
+    return jsonify(status="expense record deleted"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
